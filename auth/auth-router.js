69071ad24cd0d1a8495619f40b683f534c0e9a5d
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-15 09:21:50 +0100

    :sparkles: Write authBodyValidator
    ...to check username and password passed in

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt')
 
 router.post('/register', (req, res) => {
   // implement registration
@@ -7,5 +8,20 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
   // implement login
 });
+
+function authBodyValidator(req, res, next){
+  const { username, password } = req.body;
+
+  if(!Object.keys(req.body).length){
+    res.status(400).json({error: true, message: `Request body missing`})
+  } else if (!username || !password) {
+    res.status(400).json({error: true, message: `Required parameter missing`})
+  } else{
+    const hashedPassword = bcrypt.hashSync(password, 10)
+    req.valHashedUser = {username, password: hashedPassword}
+    req.valUser = {username, password}
+    next()
+  }
+}
 
 module.exports = router;
